27a7328493e34de807e0a854ad2abede845d372b
Graeme Meyer <graememeyer@hotmail.com>
2020-11-27 09:06:52 +0000

    Cleaned-up function commit

--- src/BXOR-DLL.c
+++ src/BXOR-DLL.c
@@ -8,25 +8,9 @@
 # define EXPORT __declspec(dllimport)
 # endif
 
-EXPORT char* __stdcall BXOR(char* inputCharArray, char* outputCharArray, char key){
-  int len = strlen(inputCharArray);
-
-  //Debuging code
-  printf("\n\n");
-  printf("Input length: %i",len);
-  printf("KEY INT: %i\t Dec: %d", key, key);
-  printf("\n\n");
-  //Debuging code
-
+EXPORT unsigned char* __stdcall modifyBytes(unsigned char* inputCharArray, int len, unsigned char key){  
   for(int i=0; i<len; i++)
   {
-      outputCharArray[i] = inputCharArray[i] ^ key;
-
-      //Debuging code
-      printf("%c\t", inputCharArray[i]);
-      printf("%c", outputCharArray[i]);
-      printf("\n");
-      //Debuging code
+      inputCharArray[i] = inputCharArray[i] ^ key;
   }
-  printf("\n"); //Debuging code
 }
